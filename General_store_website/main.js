d0f2e16b1f34d3f6dfa155ee104f142f69f826b0
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-22 21:38:10 +0530

    added delete function

--- General_store_website/main.js
+++ General_store_website/main.js
@@ -1,29 +1,19 @@
+
+
 let inventory = [];
 
-// Function to load inventory from the API and update the display
-function loadInventory() {
-    axios.get('https://crudcrud.com/api/f3c993089ff24680aa8729939a0186df/inventory')
-        .then(response => {
-            inventory = response.data;
-            displayInventory();
-        })
-        .catch(error => {
-            console.error('Error loading inventory from API:', error);
-        });
+async function loadInventory() {
+    try {
+        inventory = [];
+        const response = await axios.get('https://crudcrud.com/api/c84bc8ee31b24a0fac552f8d589e553d/inventory');
+        inventory = response.data;
+        displayInventory();
+    } catch (error) {
+        console.error('Error loading inventory from API:', error);
+    }
 }
 
-// Load inventory from local storage on page load
-if (localStorage.getItem('inventory')) {
-    inventory = JSON.parse(localStorage.getItem('inventory'));
-    displayInventory();
-    // Load the API data after displaying local storage data
-    loadInventory();
-} else {
-    // If not found in local storage, load from the API
-    loadInventory();
-}
-
-function addItem() {
+async function addItem() {
     const itemName = document.getElementById('itemName').value;
     const itemDesc = document.getElementById('itemDesc').value;
     const itemPrice = parseFloat(document.getElementById('itemPrice').value);
@@ -37,68 +27,78 @@ function addItem() {
             quantity: itemQuantity
         };
 
-        // Save the updated inventory to local storage
-        inventory.push(newItem);
-        localStorage.setItem('inventory', JSON.stringify(inventory));
-
-        // Save the new item to the API
-        axios.post('https://crudcrud.com/api/f3c993089ff24680aa8729939a0186df/inventory', newItem)
-            .then(response => {
-                // Update the local inventory with the response data
-                inventory.push(response.data);
-                displayInventory();
-            })
-            .catch(error => {
-                console.error('Error adding item to API:', error);
-            });
+        try {
+            const response = await axios.post('https://crudcrud.com/api/c84bc8ee31b24a0fac552f8d589e553d/inventory', newItem);
+            inventory.push(response.data);
+            displayInventory();
+        } catch (error) {
+            console.error('Error adding item to API:', error);
+        }
     } else {
         alert('Please fill in all item details.');
     }
 }
 
-function buyItem(itemIndex, quantity) {
+async function buyItem(itemIndex, quantity) {
     if (inventory[itemIndex].quantity >= quantity) {
         inventory[itemIndex].quantity -= quantity;
 
-        // Save the updated inventory to local storage
-        localStorage.setItem('inventory', JSON.stringify(inventory));
-
-        // Prepare the payload for updating the item quantity
-        const updatedQuantity = inventory[itemIndex].quantity;
-
-        // Make a PUT request to update the item quantity
-        axios.put(`https://crudcrud.com/api/f3c993089ff24680aa8729939a0186df/inventory/${inventory[itemIndex]._id}`, {
-            quantity: updatedQuantity
-        })
-            .then(response => {
-                // Update the local inventory with the response data
-                inventory[itemIndex].quantity = updatedQuantity;
-                displayInventory();
-            })
-            .catch(error => {
-                console.error('Error buying item:', error);
+        try {
+            const updatedQuantity = inventory[itemIndex].quantity;
+            await axios.put(`https://crudcrud.com/api/c84bc8ee31b24a0fac552f8d589e553d/inventory/${inventory[itemIndex]._id}`, {
+                quantity: updatedQuantity
             });
+            inventory[itemIndex].quantity = updatedQuantity;
+            displayInventory();
+        } catch (error) {
+            console.error('Error buying item:', error);
+        }
     }
 }
 
-
+async function deleteItem(itemIndex) {
+    try {
+        await axios.delete(`https://crudcrud.com/api/c84bc8ee31b24a0fac552f8d589e553d/inventory/${inventory[itemIndex]._id}`);
+        inventory.splice(itemIndex, 1);
+        displayInventory();
+    } catch (error) {
+        console.error('Error deleting item:', error);
+    }
+}
 
 function displayInventory() {
     const itemList = document.getElementById('itemList');
     itemList.innerHTML = '';
 
+    if (inventory.length === 0) {
+        itemList.innerHTML = '<tr><td colspan="6">No items in inventory.</td></tr>';
+        return;
+    }
+
+    const tbody = document.getElementById('itemList');
+
     inventory.forEach((item, index) => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <strong>${item.name}</strong> - ${item.description}<br>
-            Price: $${item.price.toFixed(2)}, Quantity: ${item.quantity}
-            <button onclick="buyItem(${index}, 1)">Buy 1</button>
-            <button onclick="buyItem(${index}, 2)">Buy 2</button>
-            <button onclick="buyItem(${index}, 3)">Buy 3</button>
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.name}</td>
+            <td>${item.description}</td>
+            <td>${item.price ? `$${item.price.toFixed(2)}` : 'N/A'}</td>
+            <td>${item.quantity}</td>
+            <td>
+                <button onclick="buyItem(${index}, 1)">Buy 1</button>
+                <button onclick="buyItem(${index}, 2)">Buy 2</button>
+                <button onclick="buyItem(${index}, 3)">Buy 3</button>
+                <button onclick="deleteItem(${index})">Delete</button>
+            </td>
         `;
-        itemList.appendChild(li);
+        tbody.appendChild(row);
     });
 }
 
-// Initial load of inventory
-loadInventory();
+
+try {
+    // Initial load of inventory
+    loadInventory();
+} catch (error) {
+    console.error('Error during initial load:', error);
+}

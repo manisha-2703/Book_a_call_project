d4a494bcff5b4365f841ca66caadad9894484cfe
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-25 15:00:43 +0530

    Created a server

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -1,4 +1,11 @@
-console.log("hello world");
+/*console.log("hello world");
 
-/* const fs=require('fs');
+ const fs=require('fs');
 fs.writeFileSync('Hello.text','hello from node js'); */
+
+var http=require('http');
+
+http.createServer(function(req,res){
+    res.write('name');
+    res.end()
+}).listen(4000)

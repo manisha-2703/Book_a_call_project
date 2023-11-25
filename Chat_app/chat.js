2f6118fe6492f27d9a47d6ea4ad35a60d32ebdd9
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-25 22:06:13 +0530

    Redirecting requests

---
+++ Chat_app/chat.js
@@ -0,0 +1,34 @@
+const http=require('http');
+const fs=require('fs');
+
+http.createServer(function(req,res){
+    const url=req.url;
+    const method=req.method;
+    
+    if(url==="/"){
+        res.write('<html>');
+        res.write('<head><title> My First Page</title></head>');
+        res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    if(url==="/message" && method === 'POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+
+    }
+    
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title> My First Page</title></head>');
+    res.write('<body>Welcome to my Node Js project</body>');
+    res.write('</html>');
+    res.end();
+    
+
+
+
+}).listen(4000)

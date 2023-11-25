8312727a037e4d0bde0167503afa5193f7eabe8b
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-25 18:22:54 +0530

    first response

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -1,11 +1,30 @@
 /*console.log("hello world");
 
  const fs=require('fs');
-fs.writeFileSync('Hello.text','hello from node js'); */
+fs.writeFileSync('Hello.text','hello from node js'); 
 
 var http=require('http');
 
 http.createServer(function(req,res){
     res.write('name');
-    res.end()
+    process.exit();
+}).listen(4000)
+*/
+
+const http=require('http');
+
+http.createServer(function(req,res){
+    console.log(req.url , req.method,req.headers);
+    
+    //process.exit();
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title> My First Page</title></head>');
+    res.write('<body>Welcome to my Node Js project</body>');
+    res.write('</html>');
+    res.end();
+    
+
+
+
 }).listen(4000)

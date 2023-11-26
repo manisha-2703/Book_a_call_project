abd5eb03ae452eabfa54b3bc39cdfab3ee468347
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-26 21:59:21 +0530

    used exporting

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -1,53 +1,7 @@
-/*console.log("hello world");
-
- const fs=require('fs');
-fs.writeFileSync('Hello.text','hello from node js'); 
-
-var http=require('http');
-
-http.createServer(function(req,res){
-    res.write('name');
-    process.exit();
-}).listen(4000)
-*/
-
 const http=require('http');
+const routes=require('./routes');
 
-http.createServer(function(req,res){
-    const url=req.url;
-    
-    if(url==="/home"){
-        res.write('<html>');
-        res.write('<head><title> My First Page</title></head>');
-        res.write('<body>Welcome home</body>');
-        res.write('</html>');
-        return res.end();
-
-    }
-    if(url==="/about"){
-        res.write('<html>');
-        res.write('<head><title> My First Page</title></head>');
-        res.write('<body>Welcome to About Us page</body>');
-        res.write('</html>');
-        return res.end();
-
-    }
-    if(url==="/node"){
-        res.write('<html>');
-        res.write('<head><title> My First Page</title></head>');
-        res.write('<body>Welcome to my Node Js project</body>');
-        res.write('</html>');
-        return res.end();
-
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title> My First Page</title></head>');
-    res.write('<body>Welcome to my Node Js project</body>');
-    res.write('</html>');
-    res.end();
-    
-
-
-
-}).listen(4000)
+//const server = http.createServer(routes);
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
+server.listen(4000);

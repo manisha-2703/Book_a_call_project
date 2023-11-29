680f879931a122d951122061fdd883dc0c6ae83e
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-29 18:49:27 +0530

    parseing the request

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -1,14 +1,24 @@
 //const http=require('http');
 const express= require('express');
+const bodyParser= require('body-parser')
 
 const app=express();
-app.use((req,res,next)=>{
-    console.log('in the middleware');
-    next();
-});
 
-app.use((req,res,next)=>{
-    console.log('in other middleware');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    //console.log('in other middleware');
+    res.send('<form action ="/product" method="POST"><input type ="text" name="title"><button type="submit">Add Product</button></form>')
+
+});
+app.post('/product',(req,res,next)=> {
+    console.log(req.body);
+    res.redirect('/');
+
+
+});
+app.use('/',(req,res,next)=>{
+    //console.log('in other middleware');
     res.send('<h1>Hello from Node js</h1>');
 
 });

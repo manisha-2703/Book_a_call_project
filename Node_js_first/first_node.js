eeccdbf3e816b17560c108733d73275e77ed237b
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-11-30 12:14:17 +0530

    filtered paths

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -4,24 +4,20 @@ const bodyParser= require('body-parser')
 
 const app=express();
 
+const adminroutes=require('./Routes/admin.js');
+const shoproutes=require('./Routes/shop.js');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-    //console.log('in other middleware');
-    res.send('<form action ="/product" method="POST"><input type ="text" name="title"><button type="submit">Add Product</button></form>')
+app.use('/admin',adminroutes);
+app.use('/shop',shoproutes);
 
-});
-app.post('/product',(req,res,next)=> {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 
-});
-app.use('/',(req,res,next)=>{
-    //console.log('in other middleware');
-    res.send('<h1>Hello from Node js</h1>');
-
-});
 /* const server = http.createServer(app);
 server.listen(4000); */
 app.listen(4000);

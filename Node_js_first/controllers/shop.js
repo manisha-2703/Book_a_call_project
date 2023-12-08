e3bfb619dd4c3700116c5fd00703c3ab9690e4f0
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-12-08 18:05:11 +0530

    Added delete functionality

--- Node_js_first/controllers/shop.js
+++ Node_js_first/controllers/shop.js
@@ -38,10 +38,25 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getCart(cart=>{
+    Product.fetchAll(products=>{
+      const cartProducts=[];
+      for(product of products){
+        const cartProductsData =cart.products.find(prod=>prod.id===product.id);
+        if(cartProductsData){
+          cartProducts.push({productData:product,qty:cartProductsData.qty});
+
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:cartProducts
+      });
+    })
+
+  })
+  
 };
 
 exports.postCart= (req, res, next) => {
@@ -51,6 +66,15 @@ exports.postCart= (req, res, next) => {
 
   })
   res.redirect('/cart')
+
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId,product =>{
+    Cart.deleteProduct(prodId,product.price);
+    res.redirect('/cart');
+  })
 
 };
 

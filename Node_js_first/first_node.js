22719d6d13b420e396d132dd882f187ae05c26d4
manisha-2703 <mandlipallimanisha123@gmail.com>
2023-12-14 17:56:31 +0530

    designed User product relation

--- Node_js_first/first_node.js
+++ Node_js_first/first_node.js
@@ -3,57 +3,57 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorcontroller =require('./controllers/error');
-const sequelize=require('./util/database.js')
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
 
-const adminRoutes = require('./Routes/admin');
-const shopRoutes = require('./Routes/shop');
-const contactRoutes= require('./Routes/contactus.js');
-
-
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views', 'public')));
 
-app.use('/admin',adminRoutes);
-app.use(shopRoutes);
-app.use(contactRoutes);
-
-app.use(errorcontroller.get404);
-
-sequelize.sync().then(result=>{
-    //console.log(result);
-}).catch(err=>{
-    console.log(err);
-})
-
-app.listen(3000);
-
-/* const path=require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-const adminRoutes = require('./Routes/admin.js');
-const shopRoutes = require('./Routes/shop.js');
-const contactRoutes= require('./Routes/contactus.js');
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'views', 'public')));
-
-app.use(adminRoutes);
-app.use(shopRoutes);
-app.use(contactRoutes);
- '/shop',  '/admin',
-
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'Views','404.html'));
-}); 
-app.listen(3000);
- */
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  app.listen(3000);
